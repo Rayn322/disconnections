318eade139434c9fb630dbcd5999152424c2e5fc
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2024-08-19 11:48:55 -0700

    Header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,10 @@
 	import '../app.css';
 </script>
 
-<slot />
+<header class="border-b border-neutral-200 p-4">
+	<h1 class="text-xl font-bold">Disconnections</h1>
+</header>
+
+<main class="p-4">
+	<slot />
+</main>
